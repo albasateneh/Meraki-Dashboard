f7a4c664f2b14a950ac0e2e4c6836ca9960b82c8
Sammy Albasateneh <salbasateneh@lazydogrestaurants.com>
2019-09-13 16:16:48 -0700

    Meraki Endpoints:  All Network Locations, Network Devices, Network Traffic

--- js.js
+++ js.js
@@ -1,9 +1,58 @@
 const meraki = require("meraki");
 const configuration = meraki.Configuration;
-const controller = meraki.ClientsController
 configuration.xCiscoMerakiAPIKey = "5c6a0339a3995c8002904419961c826f993edcfe";
 
-meraki.OrganizationsController.getOrganizations().then(res => console.log(res));
+//meraki.OrganizationsController.getOrganizations().then(res => console.log(res));
 
 
-   
+
+
+
+// Get Network Traffic ********NEED TO ENABLE HOSTNAME VISIBILITY********
+
+// let params = [];
+// let networkId = 'N_636696397319514887';
+// params['networkId'] = networkId;
+
+// let timespan = '7200';
+// params['timespan'] = timespan;
+
+// meraki.NetworksController.getNetworkTraffic(params)
+// .then(res => console.log(res))
+// .catch(e => console.log(e));
+
+
+
+
+
+// All Network Locations
+
+// const params = {
+//     organizationId: "361444" // Meraki Launchpad Demo
+//   };
+//   meraki.NetworksController.getOrganizationNetworks(params)
+//     .then(res => console.log(res))
+//     .catch(e => console.log(e));
+
+
+
+
+
+// Network Devices
+
+// let params = [];
+// let networkId = "N_636696397319514887"; // Sandbox Campus-SFO
+// params["networkId"] = networkId;
+
+// meraki.DevicesController.getNetworkDevices(networkId) 
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+
+
+  
+
